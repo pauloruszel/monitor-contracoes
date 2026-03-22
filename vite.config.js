740ed3eeb59275b613ce2951637af3b11b8e94ba
Paulo Ruszel <paulo.ruszel.santos@gmail.com>
2026-03-21 21:47:18 -0300

    fix: set vite base for github pages

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
+  import { defineConfig } from 'vite'
 
-export default defineConfig({
-  test: {
-    environment: 'jsdom',
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
-      all: true,
-      include: ['src/utils/*.js'],
+  export default defineConfig({
+    base: '/monitor-contracoes/',
+    test: {
+      environment: 'jsdom',
+      coverage: {
+        provider: 'v8',
+        reporter: ['text', 'html'],
+        all: true,
+        include: ['src/utils/*.js'],
+      },
     },
-  },
-})
+  })
